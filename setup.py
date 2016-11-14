bec7c820c46b06c69eb04e5ed42d96f00d91407a
Matt Chung <matt@itsmemattchung.com>
2016-11-13 17:05:00 -0800

    Bumped version for purchase-basket

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = ('requests')
 setup(
     name='softix',
     packages=['softix'],
-    version='0.0.4',
+    version='0.0.5',
     author='Matt Chung',
     author_email='matt@itsmemattchung.com',
     description='Python client library to interface with Dubai ticketing API',
